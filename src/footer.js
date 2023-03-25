5c90c5a662b9e8046c67666355a5532160471de7
Nguyễn Thành Đức <ducnguyenthanh613@gmail.com>
2023-03-25 12:58:37 +0700

    add loader css

--- src/footer.js
+++ src/footer.js
@@ -1,11 +1,10 @@
+import "./footer.css"
 import { blue, red } from "./button-styles";
 
 // ES module syntax export
 const top = document.createElement("div");
 top.innerText = "Top of footer";
 top.style = blue;
-top.style.backgroundColor = "yellow";
-top.style.padding = "20px";
 
 const bottom = document.createElement("div");
 bottom.innerText = "Bottom of footer";
